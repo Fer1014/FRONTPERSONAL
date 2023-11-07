27e15766797eb3f1eca1f8891b35f70a93e086d3
Fernando Sotelo Ramon <113149667+Fer1014@users.noreply.github.com>
2023-11-07 02:53:59 -0500

    Add files via upload

---
+++ src/app/components/tipo-usuario/tipousuario-creaedita/tipousuario-creaedita.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipousuario-creaedita',
+  templateUrl: './tipousuario-creaedita.component.html',
+  styleUrls: ['./tipousuario-creaedita.component.css']
+})
+export class TipousuarioCreaeditaComponent {
+
+}
---
+++ src/app/components/tipo-usuario/tipousuario-listar/tipousuario-listar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipousuario-listar',
+  templateUrl: './tipousuario-listar.component.html',
+  styleUrls: ['./tipousuario-listar.component.css']
+})
+export class TipousuarioListarComponent {
+
+}
